350bf5be48307295ba0b12f35ebfe0c38395f73a
nallg00d <no@way.com>
2020-02-18 19:53:55 -0500

    added more code to kilogram class from the paper

--- kilogram.py
+++ kilogram.py
@@ -3,8 +3,31 @@ import os
 
 
 class KiloGram:
+	"""
+	Base class for KiloGram from the paper. 
+	"""
+
+	def __init__(self, docs, ngrams, bucket):
+		self.docs = docs
+		self.ngrams = ngrams
+		self.bucket = bucket
+
+	def bucket_size(self):
+		return len(bucket)
+
+	def total_ngrams(self):
+		return len(ngrams)
 
 
-	def __init__(self):
+	def rollingHash(self):
+		for doc in self.docs:
+			for gram in ngrams:
+				pass
+
+	def topk_hashes(self):
+		ret
+		
+		
+
 		
 
