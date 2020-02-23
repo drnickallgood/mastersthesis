56c7f4641d2883b247323d46adc0e7747fc570b3
nallg00d <no@way.com>
2020-02-22 21:45:09 -0500

    Added pieces for rolling hash using quick select functions

--- kilogram.py
+++ kilogram.py
@@ -27,54 +27,52 @@ return top-k entries from S
 import math
 import os
 import hashlib
-import QuickSelect
+import quickselect
 
 class KiloGram:
-	"""
-	Base class for KiloGram from the paper. 
-	"""
+        """
+        Base class for KiloGram from the paper. 
+        """
 
-    hashes = list()
-    # Dictionary / hash table for hash / n-gram
-    topk_hashes = dict()        
+        hashes = list()
+        # Dictionary / hash table for hash / n-gram
+        _topk_hashes = list()
+        topk_hash_table = dict()
 
         # Document list, ngram list, bucket, hashing stride
-	def __init__(self, docs, ngrams, bucket, stride):
-		self.docs = docs
-		self.ngrams = ngrams
-		self.bucket = bucket
-        self.stride = stride
+        def __init__(self, docs, ngrams, bucket, stride):
+            self.docs = docs
+            self.ngrams = ngrams
+            self.bucket = bucket
+            self.stride = stride
 
-    def rollingHash(self):
-        for doc in self.docs:
-            for gram in ngrams:
-				hash = hashlib.sha1(gram.encode())
-                mod_hash = hash % len(self.bucket)
-                if mod_hash % self.stride == 0:
-                    hashes.append(mod_hash)
+        def rollingHash(self):
+            for doc in self.docs:
+                for gram in self.ngrams:
+                    curr_hash = hashlib.sha1(gram.encode())
+                    mod_hash = curr_hash % len(self.bucket)
+                    if mod_hash % self.stride == 0:
+                        hashes.append(mod_hash)
 
-        # Quickselect T_k from T
-        #qs = QuickSelect()
-        #a = [1,2,3,4,5,9,7,2,19]
-        #print(qs.findKthLargest(a,1))
-        #for i in range(len(a)):
-        #    lgst = qs.findKthLargest(a,i)
-        #    Get all occurences in bucket
-        #    if occurences > specified value
-        #        Add hash to new list
-        #    
-        for doc in self.docs:
-             for gram in ngrams:
-                hash = hashlib.sha1(gram.encode())
-                mod_hash = hash % len(self.bucket)
-                if mod_hash in topk_hashes:
-                      _topk_structure.append(mod_hash)
-                                        
-	def topk_hashes(self):
-                # Loop through and get hashes that occur the most
-	        
-		
-		
+            # Quickselect T_k from T
+            # Get topk hashes and append to list
+            qs = QuickSelect()
+            for i in range(10):
+                self._topk_hashes.append(qs.kFindMost(hashes,i))
+                
+            for doc in self.docs:
+                for gram in self.ngrams:
+                    curr_hash = hashlib.sha1(gram.encode())
+                    mod_hash = curr_hash % len(self.bucket)
+                    if mod_hash in topk_hashes:
+                        topk_hash_table[mod_hash] = gram
 
-		
+            return topk_hash_table
+
+
+            
+        
+        
+
+        
 
